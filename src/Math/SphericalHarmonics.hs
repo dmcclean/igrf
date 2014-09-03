dmcclean/igrf

commit b693c8a26fc58fe0490298170c8f5955e8c27d8c

Simplified addition of spherical harmonic models.

src/Math/SphericalHarmonics.hs
@@ -36,12 +36,8 @@ sphericalHarmonicModel deg r cs | valid = SphericalHarmonicModel cs''
 instance(Fractional a, Eq a) => AdditiveGroup (SphericalHarmonicModel a) where
   zeroV = SphericalHarmonicModel [[(0,0)]]
   negateV = fmap negate
-  (SphericalHarmonicModel m1) ^+^ (SphericalHarmonicModel m2) = SphericalHarmonicModel (combineCoefficients m1 m2)
+  (SphericalHarmonicModel m1) ^+^ (SphericalHarmonicModel m2) = SphericalHarmonicModel (zipWith (zipWith addPairs) m1 m2)
     where
-      combineCoefficients []       cs       = cs
-      combineCoefficients cs       []       = cs
-      combineCoefficients (c1:cs1) (c2:cs2) = combine c1 c2 : combineCoefficients cs1 cs2
-      combine = zipWith addPairs
       addPairs (g1, h1) (g2, h2) = (g1 + g2, h1 + h2)
 
 instance (Fractional a, Eq a) => VectorSpace (SphericalHarmonicModel a) where

commit 382481699bcbc6a2ebc56bff5f50c997526e3da0

Fixed regression in b693c8a that caused truncation when combining models of unequal degree.

src/Math/SphericalHarmonics.hs
@@ -39,8 +39,11 @@ sphericalHarmonicModel r cs | valid = SphericalHarmonicModel cs''
 instance(Fractional a, Eq a) => AdditiveGroup (SphericalHarmonicModel a) where
   zeroV = SphericalHarmonicModel [[(0,0)]]
   negateV = fmap negate
-  (SphericalHarmonicModel m1) ^+^ (SphericalHarmonicModel m2) = SphericalHarmonicModel (zipWith (zipWith addPairs) m1 m2)
+  (SphericalHarmonicModel m1) ^+^ (SphericalHarmonicModel m2) = SphericalHarmonicModel (combineCoefficients m1 m2)
     where
+      combineCoefficients [] cs = cs
+      combineCoefficients cs [] = cs
+      combineCoefficients (c1:cs1) (c2:cs2) = zipWith addPairs c1 c2 : combineCoefficients cs1 cs2
       addPairs (g1, h1) (g2, h2) = (g1 + g2, h1 + h2)
 
 instance (Fractional a, Eq a) => VectorSpace (SphericalHarmonicModel a) where