30ed60c1040ff9106e63509173796016cc2e6e1b
Douglas McClean <douglas.mcclean@gmail.com>
2015-10-02 16:48:53 -0400

    Progress on parser implementation.

--- src/IGRF/Parser.hs
+++ src/IGRF/Parser.hs
@@ -7,10 +7,13 @@ import Data.Text (Text)
 import qualified Data.List as L
 import qualified Data.Text as T
 
-parseModels :: Text -> [((Text, Text), SphericalHarmonicModel Double)]
-parseModels file = zipWith (\a b -> ((a,b), undefined)) headersType headersDate
+parseModels :: Text -> [((Text, Text), Maybe (SphericalHarmonicModel Double))]
+parseModels file = zipWith3 (\ty d vals -> ((ty, d), parseModel vals)) headersType headersDate values
   where
-    values = L.transpose . drop 2 $ nonComments
+    values = drop 3 . L.transpose . drop 2 $ nonComments
     headersType = drop 3 $ nonComments !! 0
     headersDate = drop 3 $ nonComments !! 1
     nonComments = fmap T.words . filter (not . T.isPrefixOf "#") $ T.lines file
+
+parseModel :: [Text] -> Maybe (SphericalHarmonicModel Double)
+parseModel values = undefined
